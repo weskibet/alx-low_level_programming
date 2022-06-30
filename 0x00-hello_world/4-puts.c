4cb181beb850c0dc845a536a53277226b7b1fe04
weskibet <wesleykbt@gmail.com>
2022-06-30 11:33:54 -0700

    Hard work pays

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,15 @@
-#!/bin/bash
 #include<stdio.h>
-
 /**
  *
- *  * main - Entry point
+ ** main - Entry point
  *
- *   *
+ **
  *
- *    * Return: Always 0 (Success)
+ ** Return: Always 0 (Success)
  *
- *     */
-
+ **/
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
