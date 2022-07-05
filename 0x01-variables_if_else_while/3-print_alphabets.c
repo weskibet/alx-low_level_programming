ad50dc943960912e7ba3822e445f446ea105523d
weskibet <wesleykbt@gmail.com>
2022-07-05 11:41:11 -0700

    no chmod

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -> Assign a random number to the variable n each
+ * and print the last digit stored of the number stored in the variable
+ * Return : Always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <=122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <=90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return(0);
+}
