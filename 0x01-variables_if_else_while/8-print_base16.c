41f107b6ba475015445c69ef26da07ba77a823b4
weskibet <wesleykbt@gmail.com>
2022-07-05 13:08:50 -0700

    Base 16 does not come easy

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - entry block
+ * description - prints all the numbers of base 16 in lowercase
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	char x;
+	for (i = 0; < 10; i++)
+		putchar(i + '0');
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
+	putchar (10);
+	retung (0);
+}
