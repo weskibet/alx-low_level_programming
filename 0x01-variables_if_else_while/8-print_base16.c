3c6a70c978ad831f5e9f8c9708181d11df5aee2e
weskibet <wesleykbt@gmail.com>
2022-07-05 13:17:31 -0700

    Ni gaain

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,16 +2,17 @@
 /**
  * main - entry block
  * description - prints all the numbers of base 16 in lowercase
- * Return: 0 on success
+ * Return: 0
  **/
 int main(void)
 {
 	int i;
 	char x;
+
 	for (i = 0; < 10; i++)
 		putchar(i + '0');
 	for (x = 'a'; x <= 'f'; x++)
 		putchar(x);
-	putchar(10);
+	putchar('\n');
 	return(0);
 }
