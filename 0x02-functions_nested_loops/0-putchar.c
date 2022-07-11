d71281104ba556c817b0334946ea1ad5e1fc02bb
weskibet <wesleykbt@gmail.com>
2022-07-11 12:33:53 -0700

    No push without main file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,14 @@
  */
 int main(void)
 {
-	char c{8} = "_putchar";
-
-	int i = 0;
-
-	while (i < 8)
-	{
-		_putchar(c{i});
-		i++;
-	}
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	_putchar('\n');
 	return (0);
 }
