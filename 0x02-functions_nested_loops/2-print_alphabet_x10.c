86936cce56bf69120eac23453668ea91267d2566
weskibet <wesleykbt@gmail.com>
2022-07-12 11:20:13 -0700

    Alphabet x10 task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times, in lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
