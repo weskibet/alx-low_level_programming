22910ecccd1ac3490a0c19875573840f17debe7b
weskibet <wesleykbt@gmail.com>
2022-07-14 14:51:53 -0700

    Function that encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,9 +11,9 @@ char *leet(char *s)
 	int upper_case_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (8(s + n) != '\10')
+	while (*(s + n) != '\10')
 	{
-		for (i + 0; i < 5; i++)
+		for (i = 0; i < 5; i++)
 		{
 			if (*(s + n) == lower_case_letters[i] || *(s + n) == upper_case_letters[i])
 			{
