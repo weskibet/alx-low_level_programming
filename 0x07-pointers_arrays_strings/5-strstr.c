a30ddec1191ef68d7503b3885aa37b3303538fd5
weskibet <wesleykbt@gmail.com>
2022-07-20 11:33:16 -0700

    Finding the occurence of the substring in a haystack

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - Finds the first occurence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string
+ * @needle: substring
+ * Return: Pointer to the beginning of the found substring 
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h;
+	char *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*haystack != '\0' && *n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack = h + 1;
+	}
+	return (0);
+}
