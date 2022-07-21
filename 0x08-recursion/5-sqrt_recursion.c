b1b87a496f8a2c1cb92f9e05002aa59969829ef1
weskibet <wesleykbt@gmail.com>
2022-07-21 12:29:24 -0700

    No retreat no surrender

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,21 @@
 #inclue "main.h"
-
+/**
+ * power_operation - returns the natural dquare root of a number
+ * @n: input number.
+ * @c: iterator.
+ * Return: square root of -1.
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
 /**
  * _sqrt_recursion -returns the natural square root of a number
  * @n: input number
